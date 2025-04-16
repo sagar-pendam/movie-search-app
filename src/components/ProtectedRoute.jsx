e216ff6c36903599813587a683ad01bb5d1d1f94
pendamsagar <pendamsagar3@gmail.com>
2025-04-16 19:05:07 +0530

    syntax correction

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import React ,{useContext}from 'react'
+import React,{useContext} from 'react'
 import { Navigate } from 'react-router-dom'
 import MovieContext from '../context/MovieContext'
 function ProtectedRoute({children}) {
